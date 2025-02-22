c98f6902324090c3c7f579c333ef5b764c270af9
CS <chris.stewart132@gmail.com>
2025-02-22 13:46:51 +1300

    .bat

---
+++ ImageToImageCNN/data/video_to_images_train.bat
@@ -0,0 +1,4 @@
+REM extracting frames from video files in train/input train/output test... etc
+python video_to_images.py --video_dir train/input --output_dir train/input
+python video_to_images.py --video_dir train/output --output_dir train/output
+pause
---
+++ ImageToImageCNN/data/video_to_images_validation.bat
@@ -0,0 +1,4 @@
+REM extracting frames from video files in train/input train/output test... etc
+python video_to_images.py --video_dir validation/input --output_dir validation/input
+python video_to_images.py --video_dir validation/output --output_dir validation/output
+pause
