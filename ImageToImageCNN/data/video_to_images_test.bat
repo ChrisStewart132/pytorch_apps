67f25ec322d5c5cfe129354522cc61c474258182
CS <chris.stewart132@gmail.com>
2025-02-27 13:01:44 +1300

    example trained models

--- ImageToImageCNN/data/video_to_images_test.bat
+++
@@ -1,4 +0,0 @@
-REM extracting frames from video files in train/input train/output test... etc
-python video_to_images.py --video_dir test/input --output_dir test/input
-python video_to_images.py --video_dir test/output --output_dir test/output
-pause
